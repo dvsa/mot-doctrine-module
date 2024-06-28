d7847026c45a0410502a94e058ae7a5e0e6f7493
Andrew Lai <andrew.lai@bjss.com>
2024-06-28 15:57:18 +0100

    chore: removing return types

--- src/Module.php
+++ src/Module.php
@@ -4,17 +4,11 @@ namespace DvsaDoctrineModule;
 
 class Module
 {
-    /**
-     * @return array
-     */
     public function getConfig(): array
     {
         return include __DIR__ . '/../config/module.config.php';
     }
 
-    /**
-     * @return array
-     */
     public function getModuleDependencies(): array
     {
         return array();
